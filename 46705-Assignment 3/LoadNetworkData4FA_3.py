2ba7f94e4e959ef11c39bb79ca918e8491ed8e31
MatheoChalas <145017925+MatheoChalas@users.noreply.github.com>
2024-04-23 10:44:05 +0200

    Add files via upload

--- 46705-Assignment 3/LoadNetworkData4FA_3.py
+++ 46705-Assignment 3/LoadNetworkData4FA_3.py
@@ -37,9 +37,11 @@ def LoadNetworkData4FA(filename):
         Ybus[ind_fr,ind_to]+= -Y_se
         Ybus[ind_to,ind_fr]+= -Y_se
         
+
     #Get data from transformers   
     for line,i in zip(tran_data,range(len(line_data),N_branches)):
         bus_fr, bus_to, id_, R,X,n,ang1,fr_co, to_co, X2, X0 = line #unpack
+        
         ind_fr = bus_to_ind[bus_fr]    
         ind_to = bus_to_ind[bus_to] 
         Zeq = 1j*X; Yeq = 1/Zeq #We neglect the resistance
@@ -65,24 +67,15 @@ def LoadNetworkData4FA(filename):
 
         #br_Ymat[i]=Yps_mat
 
+
     #Get data from generators
     for line in gen_data:
         bus_nr, MVA_size, p_gen,X, X2, X0, Xn, GRND = line #new     
-        ind_fr = bus_to_ind[bus_fr]    
-        ind_to = bus_to_ind[bus_to] 
-        Zeq = 1j*X; Yeq = 1/Zeq #We neglect the resistance
-        Yps_mat = np.zeros((2,2),dtype=complex)
-        ### adminttance matrix
-        Yps_mat[0,0] = Yeq
-        Yps_mat[0,1] = -Yeq
-        Yps_mat[1,0] = -Yeq
-        Yps_mat[1,1] = Yeq
-        # indices
-        ind_ = np.array([ind_fr,ind_to])
+        ind_bus = bus_to_ind[bus_nr]    
+        Zeq = 1j*X2; Yeq = 1/Zeq #We neglect the resistance. We consider X2
+
         #update
-        Ybus[np.ix_(ind_,ind_)] += Yps_mat
-        br_f[i] = ind_fr
-        br_t[i] = ind_to
+        Ybus[ind_bus,ind_bus] += Yeq
 
 #Ybus2 - Negative sequence      
     Ybus2 = np.zeros((N,N),dtype=complex) #Negative sequence   
@@ -127,26 +120,17 @@ def LoadNetworkData4FA(filename):
         
 
         #br_Ymat[i]=Yps_mat
+        
 
     #Get data from generators
     for line in gen_data:
         bus_nr, MVA_size, p_gen,X, X2, X0, Xn, GRND = line #new     
-        ind_fr = bus_to_ind[bus_fr]    
-        ind_to = bus_to_ind[bus_to] 
+        ind_bus = bus_to_ind[bus_nr]    
         Zeq = 1j*X2; Yeq = 1/Zeq #We neglect the resistance. We consider X2
-        Yps_mat = np.zeros((2,2),dtype=complex)
-        ### adminttance matrix
-        Yps_mat[0,0] = Yeq
-        Yps_mat[0,1] = -Yeq
-        Yps_mat[1,0] = -Yeq
-        Yps_mat[1,1] = Yeq
-        # indices
-        ind_ = np.array([ind_fr,ind_to])
+
         #update
-        Ybus2[np.ix_(ind_,ind_)] += Yps_mat
-        br_f[i] = ind_fr
-        br_t[i] = ind_to
-    
+        Ybus2[ind_bus,ind_bus] += Yeq
+ 
  #Ybus0 - Zero sequence    
     Ybus0 = np.zeros((N,N),dtype=complex) #Zero sequence
     #Get data from lines
@@ -162,29 +146,20 @@ def LoadNetworkData4FA(filename):
         Ybus0[ind_to,ind_to]+= Y_se 
         Ybus0[ind_fr,ind_to]+= -Y_se
         Ybus0[ind_to,ind_fr]+= -Y_se
-        
+
     #Get data from generators
     for line in gen_data:
         bus_nr, MVA_size, p_gen,X, X2, X0, Xn, GRND = line #new     
-        ind_fr = bus_to_ind[bus_fr]    
-        ind_to = bus_to_ind[bus_to]
+        ind_bus = bus_to_ind[bus_nr]    
+
         if GRND==1:
             Zeq = 1j*(X0+3*Xn); Yeq = 1/Zeq #We neglect the resistance. We consider X0 and Xn
         else:
             Zeq = 1j*(X0); Yeq = 1/Zeq #We neglect the resistance. We consider X0 (Xn generates an open circuit)#CHECK IT
-        Yps_mat = np.zeros((2,2),dtype=complex)
-        ### adminttance matrix
-        Yps_mat[0,0] = Yeq
-        Yps_mat[0,1] = -Yeq
-        Yps_mat[1,0] = -Yeq
-        Yps_mat[1,1] = Yeq
-        # indices
-        ind_ = np.array([ind_fr,ind_to])
+        
         #update
-        Ybus0[np.ix_(ind_,ind_)] += Yps_mat
-        br_f[i] = ind_fr
-        br_t[i] = ind_to
-    
+        Ybus0[ind_bus,ind_bus] += Yeq
+           
     #Get data from transformers   
     for line,i in zip(tran_data,range(len(line_data),N_branches)):
         bus_fr, bus_to, id_, R,X,n,ang1,fr_co, to_co, X2, X0 = line #unpack
@@ -227,8 +202,64 @@ def LoadNetworkData4FA(filename):
         
 
         #br_Ymat[i]=Yps_mat
+
+    """
+    #Get data from generators
+    for line in gen_data:
+        bus_nr, MVA_size, p_gen,X, X2, X0, Xn, GRND = line #new     
+        ind_bus = bus_to_ind[bus_nr]    
+        Z_gen=1j*X  ;Y_gen=1/Z_gen
+        Z_gen2=1j*X2;Y_gen2=1/Z_gen2
+        
+        
+        if GRND==1:
+            Z_gen0=1j*(X0+3*Xn) #We neglect the resistance. We consider X0 and Xn
+        else:
+            Z_gen0=1j*(X0); #We neglect the resistance. We consider X0 (Xn generates an open circuit)#CHECK IT
+        Y_gen0=1/Z_gen0
+        
+        Ybus [ind_bus,ind_bus]+=Y_gen
+        
+        Ybus2 [ind_bus,ind_bus]+=Y_gen2
+  
+        Ybus0 [ind_bus,ind_bus]+=Y_gen0
+
+        
+        
+    for line in tran_data :
+        bus_fr, bus_to, id_, R,X,n,ang1,fr_co, to_co, X2, X0 = line #unpack
+        ind_fr = bus_to_ind[bus_fr]    
+        ind_to = bus_to_ind[bus_to]
+        Z_tr=1j*X  ;Y_tr=1/Z_tr
+        Z_tr2=1j*X2;Y_tr2=1/Z_tr2
+        Z_tr0=1j*X0;Y_tr0=1/Z_tr0
+        
+        Ybus [ind_fr,ind_fr]+=Y_tr
+        Ybus [ind_to,ind_to]+=Y_tr
+        Ybus [ind_fr,ind_to]+= -Y_tr
+        Ybus [ind_to,ind_fr]+= -Y_tr
+        
+        Ybus2 [ind_fr,ind_fr]+=Y_tr2
+        Ybus2 [ind_to,ind_to]+=Y_tr2
+        Ybus2 [ind_fr,ind_to]+= -Y_tr2
+        Ybus2 [ind_to,ind_fr]+= -Y_tr2
+        
+        if fr_co==2 and to_co==1:
+            Ybus0 [ind_fr,ind_fr]+=Y_tr0
+            Ybus0 [ind_to,ind_to]+=Y_tr0
+            Ybus0 [ind_fr,ind_to]+= -Y_tr0
+            Ybus0 [ind_to,ind_fr]+= -Y_tr0
+            #We neglect the resistance. We consider X0 (OPEN CIRCUIT-INFINIT IMPEDANCE)WE NEED TO CONSIDER X0 in diagonal
+        elif fr_co==2 and to_co==3:
+            Ybus0 [ind_fr,ind_fr]+=Y_tr0 #We neglect the resistance. We consider X0 (OPEN CIRCUIT-INFINIT IMPEDANCE) WE NEED TO CONSIDER X0 in diagonal
+        elif fr_co==3 and to_co==2:
+            Ybus0 [ind_to,ind_to]+=Y_tr0 #We neglect the resistance. We consider X0 (OPEN CIRCUIT-INFINIT IMPEDANCE) WE NEED TO CONSIDER X0 in diagonal
+     """  
+        
+        
+    
     Zbus0 = np.linalg.inv(Ybus0)
     Zbus1 = np.linalg.inv(Ybus)
     Zbus2 = np.linalg.inv(Ybus2)
-    
+    print(Zbus0,Zbus1,Zbus2)
     return   
