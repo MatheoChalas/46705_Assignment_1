96ca207a6290fe34e5ec1d23dde83c5c55c341df
MatheoChalas <145017925+MatheoChalas@users.noreply.github.com>
2024-04-26 17:03:34 +0200

    Add files via upload

--- 46705-Assignment 3/main4FA.py
+++ 46705-Assignment 3/main4FA.py
@@ -8,12 +8,13 @@ import LoadNetworkData4FA as lnd4fa # load the network data to global variables
 filename = "./TestSystem4FA.txt"
 lnd4fa.LoadNetworkData4FA(filename) # makes Zbus0 available as lndfa.Zbus0 etc.
 # Carry out the fault analysis ... 
-FaultBus = 3
+FaultBus = 2
 # FaultType: 0 = 3-phase balanced fault; 1 = Single Line-to-Ground fault;
 #            2 = Line-to-Line fault;     3 = Double Line-to-Ground fault.
-FaultType = 1
+FaultType = 3
 FaultImpedance = 0 # (in pu) 
-PrefaultVoltage = 1.000 # (in pu)
+PrefaultVoltage = 1.0 # (in pu)
+
 # Iph: phase current array (0: phase a; 1: phase b; 2: phase c). 
 # Vph_mat: phase line-to-ground voltages (rows: busses; columns: phases a, b, c).
 Iph,Vph_mat = fa.FaultAnalysis(lnd4fa.Zbus0,lnd4fa.Zbus1,lnd4fa.Zbus2,lnd4fa.bus_to_ind, 
